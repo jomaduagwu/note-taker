6943b89d9c3e562a1deb367154031319aacfa33c
Ms Eko <jomaduagwu@gmail.com>
2023-05-05 00:14:40 -0500

    updated code

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,22 @@
 const express = require('express')
+const path = require('path');
+const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// The following HTML routes should be created:
-// GET /notes should return the notes.html file.
-// GET * should return the index.html file.
-app.get('/', (req, res) => {
-    res.send()
-})
 
-app.get('/notes', (req, res) => {
-    res.send()
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/routes/api', apiRoutes);
+app.use('/routes/html', htmlRoutes);
 
 
 
-// The following API routes should be created:
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+app.listen(PORT, () =>
+console.log(`Express server listening on port ${PORT}`));
