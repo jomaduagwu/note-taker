3d96b0576a750727ea7cf31b678c065764895bb8
Ms Eko <jomaduagwu@gmail.com>
2023-05-08 11:27:05 -0500

    fixed code

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,16 +5,16 @@ const router = express.Router();
 
    // GET /notes should return the notes.html file.
    router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
     router.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
     // GET * should return the index.html file.
     router.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
 
